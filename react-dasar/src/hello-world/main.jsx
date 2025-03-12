ed4d9ad3f22976c1d9d98b58d6652ca42c0e746b
fariez11 <fariezilham03@gmail.com>
2025-03-12 14:20:13 +0700

    learn about Pure Compoenent

--- react-dasar/src/hello-world/main.jsx
+++ react-dasar/src/hello-world/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client"
 import HelloWorld from "./helloWorld.jsx"
 import Container from "./container.jsx"
 import TodoList from "../todo/todoList.jsx"
+import Table from "../table/table.jsx"
 
 
 createRoot(document.getElementById("root")).render(
     <Container>
         <HelloWorld />
         <TodoList />
+        <Table />
     </Container>
 )
