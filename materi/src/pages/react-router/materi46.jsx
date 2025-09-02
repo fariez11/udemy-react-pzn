71ee5a63f79618c6af6e73a8c8a2976a58ad21eb
Fariez Alviansyah <fariez.alviansyah@ibm.com>
2025-09-02 16:09:15 +0700

    update structur project

---
+++ materi/src/pages/react-router/materi46.jsx
@@ -0,0 +1,38 @@
+import TabbedCard from "../../components/tabCard";
+
+export default function Materi46() {
+    return (
+        <TabbedCard noteContent={<Note />} resultContent={<Result />} />
+    )
+}
+
+function Note() {
+    return (
+        <>
+            <h3>Router Library</h3>
+            <ul>
+                <li>Salah satu hal yang bisa kita lakukan dibanding mengganti-ganti halaman secara manual, adalah menggunakan library khusus untuk itu, yaitu Router Library</li>
+                <li>Router Library adalah library yang digunakan agar Component yang ditampilkan bisa berbeda-beda tergantung dari URL yang diakses</li>
+                <li>Dengan begitu, kita bisa fokus membuat Component untuk halaman, tidak perlu memikirkan lagi kompleksitas bagaimana cara melakukan routing (menentukan Component mana yang ditampilkan untuk URL diakses)</li>
+                <li>Untungnya, banyak sekali Router Library yang tersedia untuk React</li>
+            </ul>
+
+            <h6>React Router</h6>
+            <ul>
+                <li>Ada banyak sekali Router Library yang bisa kita gunakan untuk React</li>
+                <li><a href="https://react.libhunt.com/libs/router/react" target="_blank">Daftar Router Library untuk React</a></li>
+                <li>Namun pada materi ini, kita akan menggunakan salah satu Router Library yang populer, yaitu adalah React Router</li>
+                <li><a href="https://reactrouter.com" target="_blank">React Router Official Website</a></li>
+                <li>React Router adalah salah satu library untuk routing yang open source dan saat ini sangat populer digunakan di ekosistem React</li>
+                <li><a href="https://github.com/remix-run/react-router" target="_blank">React Router GitHub Repository</a></li>
+            </ul>
+
+        </>
+    )
+}
+
+function Result() {
+    return (
+        <></>
+    )
+}
--- materi/src/routes.jsx
+++ materi/src/routes.jsx
@@ -38,6 +38,7 @@ import Materi39 from "./pages/react-dasar/materi39";
 import Materi40 from "./pages/react-dasar/materi40";
 import IntroReactRouter from "./pages/react-router/intro";
 import Materi45 from "./pages/react-router/materi45";
+import Materi46 from "./pages/react-router/materi46";
 
 export const defaultMenu = [
     { path: "/", element: <Introduction /> },
@@ -84,5 +85,6 @@ export const reactDasar = [
 export const reactRouter = [
     { path: "/materi-43", element: <IntroReactRouter />, label: 'intro' },
     { path: "/materi-45", element: <Materi45 />, label: 'pengenalan SPA' },
+    { path: "/materi-46", element: <Materi46 />, label: 'router library' },
 ]
 
